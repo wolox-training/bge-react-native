865431916fbda2a764a113090e8c475fa1e8ca5c
Brian Gerez <brian.gerez@wolox.com.ar>
2020-06-08 18:55:05 -0300

    corrections in comment styles

--- src/app/screens/BookDetail/components/CommentsList/components/Comment/styles.ts
+++ src/app/screens/BookDetail/components/CommentsList/components/Comment/styles.ts
@@ -1,20 +1,22 @@
 import { StyleSheet } from 'react-native';
 import { CHARCOAL } from '@constants/colors';
 
+const PHOTO_SIZE = 40;
+
 const styles = StyleSheet.create({
   commentContainer: {
     flexDirection: 'row',
     marginBottom: 20
   },
   photo: {
-    borderRadius: 40,
-    height: 40,
+    borderRadius: PHOTO_SIZE / 2,
+    height: PHOTO_SIZE,
     marginTop: 5,
-    width: 40
+    width: PHOTO_SIZE
   },
   description: {
     flex: 1,
-    paddingLeft: 15
+    marginLeft: 15
   },
   name: {
     fontSize: 17,
